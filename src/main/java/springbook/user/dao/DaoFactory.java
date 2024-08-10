7a10e6be8c86879b8b0469645518b02855ff03e1
신승열 <shinseungyeol0227@gmail.com>
2024-08-10 15:20:29 +0900

    스프링으로 전환

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,15 +1,23 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * DAO 의 객체 생성 방법을 결정하고, 오브젝트를 반환하는 팩토리 객체
  * 오브젝트의 관계를 정의하는 책임을 맡고 있음
  */
+
+@Configuration
 public class DaoFactory {
+
+  @Bean
   public UserDao userDao() {
     UserDao userDao = new UserDao(connectionMaker());
     return userDao;
   }
 
+  @Bean
   public ConnectionMaker connectionMaker() {
     return new SimpleConnectionMaker();
   }
--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,13 +1,16 @@
 package springbook.user.dao;
 
 import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 public class UserDaoTest {
 
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
-    DaoFactory daoFactory = new DaoFactory();
-    UserDao dao = daoFactory.userDao();
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+    UserDao dao = context.getBean("userDao", UserDao.class);
 
     User user = new User();
     user.setId("whiteship");
@@ -16,13 +19,13 @@ public class UserDaoTest {
 
     dao.add(user);
 
-    System.out.println(STR."\{user.getId()} 등록 성공");
+    System.out.println(user.getId() +  " 등록 성공");
 
     User user2 = dao.get(user.getId());
     System.out.println(user2.getName());
     System.out.println(user2.getPassword());
 
-    System.out.println(STR."\{user.getId()} 조회 성공");
+    System.out.println(user.getId() +  " 조회 성공");
   }
 
 }
