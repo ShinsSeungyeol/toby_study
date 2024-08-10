843ae5d9cb851f004be2a9e7c3732afb80847415
신승열 <shinseungyeol0227@gmail.com>
2024-08-10 13:04:00 +0900

    connection Maker 추후 중복을 대비하여 메소드 추출

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -2,11 +2,15 @@ package springbook.user.dao;
 
 /**
  * DAO 의 객체 생성 방법을 결정하고, 오브젝트를 반환하는 팩토리 객체
+ * 오브젝트의 관계를 정의하는 책임을 맡고 있음
  */
 public class DaoFactory {
   public UserDao userDao() {
-    ConnectionMaker connectionMaker = new SimpleConnectionMaker();
-    UserDao userDao = new UserDao(connectionMaker);
+    UserDao userDao = new UserDao(connectionMaker());
     return userDao;
+  }
+
+  public ConnectionMaker connectionMaker() {
+    return new SimpleConnectionMaker();
   }
 }
