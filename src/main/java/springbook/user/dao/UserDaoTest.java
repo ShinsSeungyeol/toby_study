a5283f1deb5a1afa6f7ac2b0770564bfefbbbf48
신승열 <shinseungyeol0227@gmail.com>
2024-08-12 21:31:33 +0900

    count 테스트 메소드 추가

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -35,4 +35,26 @@ public class UserDaoTest {
     assertEquals(user.getPassword(), foundUser.getPassword());
   }
 
+  @Test
+  public void count() throws SQLException {
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserDao dao = context.getBean("userDao", UserDao.class);
+
+
+    User user1 = new User("gyumee", "박성철", "springno1");
+    User user2 = new User("leegw700", "이길원", "springno2");
+    User user3 = new User("bumjin", "박범진", "springno3");
+
+    dao.deleteAll();
+    assertEquals(dao.getCount(),0);
+
+    dao.add(user1);
+    assertEquals(dao.getCount(),1);
+
+    dao.add(user2);
+    assertEquals(dao.getCount(),2);
+
+    dao.add(user3);
+    assertEquals(dao.getCount(),3);
+  }
 }
